40ad5344a6f63be7caf63d960ef54d39df329459
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-06-23 17:55:57 -0500

    Favicon Is Coming Soon

--- JS/site.js
+++ JS/site.js
@@ -1,3 +1,42 @@
-function helloworld() {
-    alert('Hello Javascript');
+// Get String From The Page
+// controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("stringInput").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+    
+    
+
+}
+
+
+// Write Function That Reverses The String
+// logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    // reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+}
+
+
+
+// Display The Message To The User
+// view function
+function displayString(revstring) {
+
+    //write to page
+    document.getElementById("message").innerHTML = `Your String Reversed Is: ${revstring}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
